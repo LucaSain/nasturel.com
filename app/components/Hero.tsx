4872c4b81411a0f813737fe38edf52807074ffb2
LucaSain <lucasainenco2@gmail.com>
2024-07-04 18:53:41 +0300

    fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 id="test" className="text-5xl font-bold">
-            Sal'tare
+            Saltare
           </h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
