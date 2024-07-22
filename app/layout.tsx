e51964ab0d3b61dcba547fceb822aafe024ca3e0
LucaSain <lucasainenco2@gmail.com>
2024-07-22 12:42:14 +0300

    update arrow

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons: { icon: "/favicon.ico" },
   title: "Festivalul Nasturel",
   description:
     "Cel mai tare festival pentru copii din Romania! In curand la Sibiu!",
